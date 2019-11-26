e9e47d1894608a6bddcce84869ddb40317004381
Seth Frey <moctodliamg@gmail.com>
2019-11-25 23:55:52 -0800

    mature code: switched to openpyxl to preserve xl features like style and data validation.  introduced other bugs like inability to processs several sheets

--- break_compounds.py
+++ break_compounds.py
@@ -1,57 +1,124 @@
-""" 
-take a multisheet xlsx and for each sheet break its compound statements into more rows of data
+# pylint: disable = invalid-name, superfluous-parens, bad-whitespace, using-constant-test
+"""
+Take a multisheet xlsx and for each sheet break its compound statements into more rows of data
 Usage: python break_compounds.py
 """
 import sys
-import csv
-import numpy as np
-import pandas as pd
+import copy
 import nltk
-from nltk import sent_tokenize
 #nltk.download('punkt')
-import coding_helper
+from openpyxl import load_workbook
+from openpyxl.utils.cell import column_index_from_string
+#import coding_helper
 
 input_file = sys.argv[1]
 output_file = sys.argv[2]
 
-### HELPER
-def break_compounds_sheet( sheet, verbose=False ):
-    #### counters for sanity, debugging, and info
-    row_count = 0
-    row_count_n = 0
-    rows_out = []
-    for i, row in sheet.iterrows():  # For each row of the sheet ...
-        #print( "for each row", type( row ))
-        an_is = row["Institutional Statement"]
-        ### ... break into several IS's if necessary
-        ###     this code retokenizes the same string each sheet. not worth optimizing.
-        an_is = an_is.replace(r'\n', ' ')
-        an_is = an_is.replace(r'\\', ' ')
-        iss = nltk.sent_tokenize( an_is )
-        row_count += 1
-        if len(iss) == 1:
-            rows_out.append( row )
-            row_count_n += 1
-        else:
-            ### if IS was compund, break it into identical rows
-            for an_is_new in iss:
-                row_new = row.copy()
-                row_new['Institutional Statement'] = an_is_new
-                row_new['Text Type'] = 'IS_recovered'
-                rows_out.append( row_new )
-                row_count_n += 1
-    print("Sheet {} had {} rows, now has {} rows".format( sheet_name, row_count, row_count_n ))
-    sheet_out = pd.DataFrame( rows_out )
-    return( sheet_out )
+### HELPERS
 
-### SCRIPT
-#read document
-xls = pd.ExcelFile( input_file )
-sheets = xls.sheet_names
-for sheet_name in sheets:
-    ### only stretch sheets with data
-    if not sheet_name.startswith('statements_'):
-        continue
-    sheet = pd.read_excel(input_file, sheet_name)
-    sheet_out = break_compounds_sheet( sheet ) # <- creates new sheet with compunds fractured into new rows
-    sheet_out.to_excel(output_file, sheet_name=sheet_name)
+def get_col_headers(ws):
+    """
+    for excel style worksheet, get dict mapping col names to numeric indices for use in openpyxl
+    """
+    header_row = ws[1]
+    # extract value of first row (header string name)
+    row_headers = [cell.value for cell in header_row]
+    # extract column name (excel standard alpha code)
+    row_cols = [cell.column for cell in header_row]
+    # turn alpha code to number of col
+    row_idxs = [column_index_from_string(cell)-1 for cell in row_cols]
+    # keys are familiar header names, values are numeric indices, 0-indexed
+    ref = dict(zip(row_headers, row_idxs))
+    return(ref)
+
+def get_col_headers_workbook(workbook):
+    """
+    for workbook whose sheets all have the same columns,
+    get dict mapping col names to numeric indices for use in openpyxl
+    """
+    ws1 = workbook.active
+    headers = get_col_headers(ws1)
+    return( headers )
+
+if __name__ == "__main__":
+    wb = load_workbook(input_file)
+    headerRef = get_col_headers_workbook(wb)
+    #print( headerRef )
+
+    rows_to_change = []
+    for sheet in wb.worksheets:
+        if not sheet.title.startswith( 'statements_' ):
+            continue
+        print( 'SHEET', sheet.title )
+        rows_original = 0
+        rows_added = 0
+        #print(sheet)
+        for i, row in enumerate(sheet.iter_rows(), start=1):
+            #assert( row == sheet[i],
+            #   "enumerate here is supposed to give the index of the row in sheet" )
+            an_is = row[ headerRef['Institutional Statement'] ].value
+            if an_is is None:
+                break # stop at the end of the sheet
+            an_is = an_is.replace(r'\\', ' ') # for minecraft
+            if an_is.startswith('='):
+                an_is = '\\' + an_is
+                an_is = an_is.replace(r'.**', '. ') # for reddit: sentences followed by markdown
+            an_is = an_is.replace(r'.*', '. ') # for reddit
+            an_is = an_is.replace(r'\n\n**', '. ') # for reddit: double \n+bullets instead of .'s
+            an_is = an_is.replace(r'\n\n*', '. ') # for reddit
+            an_is = an_is.replace(r'\n\n-', '. ') # for reddit
+            an_is = an_is.replace(r'\n**', '. ') # for reddit
+            an_is = an_is.replace(r'\n*', '. ') # for reddit
+            an_is = an_is.replace(r'\n', ' ') # for reddit
+            iss = nltk.sent_tokenize( an_is )
+            if len( iss ) == 1: # non-compound statement
+                row[ headerRef['Institutional Statement'] ].value = an_is
+            else: # compound statement
+                #print()
+                # rows_to_change is the rows to be cahnged in the orignal sheet
+                #   old compounds should be removed, while new ones should be added
+                #   each element is a 3-tuple: row #, add/remove, row object
+                #   The order that things get added to rows to change matters:
+                #   The old row-to-be-deleted has to get appended after its parts-to-be-added
+                for an_is_part in iss:
+                    #print(i, an_is_part )
+                    # This copy is important.
+                    # Both the row and cells have to be deep copied
+                    new_row = [copy.copy(cell) for cell in row]
+                    new_row[ headerRef['Institutional Statement'] ].value = an_is_part
+                    new_row[ headerRef['Text Type'] ].value = 'decompounded'
+                    rows_to_change.append( (i, 1, new_row) )
+                    rows_added += 1
+                rows_to_change.append( (i, 0, row) )
+                rows_added -= 1
+        rows_original = i  #pylint: disable=undefined-loop-variable
+        # this reverse is important.
+        #   it makes the row numbers stay the same for each row that has to be changed.
+        rows_to_change.reverse()
+        # perform changes
+        for i, action, row in rows_to_change:
+            #print(i, action, row)
+            if action:
+                sheet.insert_rows(i)
+                # have to go cell by cell
+                #  from https://stackoverflow.com/questions/33920108/writing-to-row-using-openpyxl
+                for col, cell in enumerate(row, start=1):
+                    sheet.cell(row=i, column=col).value = cell.value
+                    sheet.cell(row=i, column=col).style = cell.style
+            else:
+                sheet.delete_rows(i)
+        # Print it out
+        print("Rows starting: {} Rows added: {}".format( rows_original, rows_added ))
+        if False:
+            for i, row in enumerate(sheet.iter_rows(), start=1):
+                an_is = row[ headerRef['Institutional Statement'] ].value
+                if an_is is None:
+                    break # stop at the end of the sheet
+                print( i, an_is )
+        ### now do sheet level edits
+        # extend data validations to new rows
+        for validation in sheet.data_validations.dataValidation:
+            validation.sqref.ranges[0].expand(down=rows_added)
+        ### currently only works for one sheet.  some bug makes it fail for others.
+        break
+    wb.save( output_file )
